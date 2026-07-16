9ae99823a45d29f65cd9f9125887fc0e6468b3b8
leyden100 <leyden100@gmail.com>
2026-07-16 21:52:35 +0100

    Automated backup 2026-07-16 21:52:35

--- scripts/Backup.sh
+++ scripts/Backup.sh
@@ -1,4 +1,4 @@
 cd ~/eclipse-workspace
-tar -czf OCI_Wrapper_backup_2026_07_015_23_00_Github_backup_complete.tar.gz OCI_Wrapper
-dpkg --get-selections > ~/installed-packages.txt
+tar -czf OCI_Wrapper_backup_2026_07_17_21_30_Level_Parser_complete.tar.gz OCI_Wrapper
+dpkg --get-selections > /home/leyden100/eclipse-workspace/OCI_Wrapper/include/eclipse-workspaceinstalled-packages.txt
 
