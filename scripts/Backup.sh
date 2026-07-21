b5361a3a6b725ff413f0c6514680856cf12cbf14
leyden100 <leyden100@gmail.com>
2026-07-21 21:02:26 +0100

    Automated backup 2026-07-21 21:02:26

--- scripts/Backup.sh
+++ scripts/Backup.sh
@@ -1,4 +1,4 @@
 cd ~/eclipse-workspace
-tar -czf OCI_Wrapper_backup_2026_07_21_22_00_json_write_complete.tar.gz OCI_Wrapper
+tar -czf OCI_Wrapper_backup_2026_07_21_21_00_cache_row_count_fix.tar.gz OCI_Wrapper
 dpkg --get-selections > /home/leyden100/eclipse-workspace/OCI_Wrapper/include/eclipse-workspaceinstalled-packages.txt
 
