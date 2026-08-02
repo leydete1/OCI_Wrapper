8f329f97d03990a335cc87e6484d97e7bb77c7a9
leyden100 <leyden100@gmail.com>
2026-08-02 16:17:52 +0100

    Automated backup 2026-08-02 16:17:52

--- scripts/Git_Push.sh
+++ scripts/Git_Push.sh
@@ -4,6 +4,7 @@
 cd ~/eclipse-workspace/OCI_Wrapper || exit 1
 
 git add .
+git reset wallet/
 
 if ! git diff --cached --quiet; then
     git commit -m "Automated backup $(date '+%Y-%m-%d %H:%M:%S')"
